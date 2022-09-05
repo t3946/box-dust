0ec0724da937224367f0584370850b7172e5cdbc
Vladislav <svialence@yandex.ru>
2022-09-05 12:19:07 +0300

    remove unused

--- server/src/utils/baseUrl.ts
+++
@@ -1,8 +0,0 @@
-function getBaseUrl() {
-  const domain = process.env.domain;
-  const protocol = process.env.ssl === '1' ? 'https' : 'http';
-
-  return protocol + '://' + domain;
-}
-
-export default getBaseUrl;
