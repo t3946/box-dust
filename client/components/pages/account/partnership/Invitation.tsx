071c9701d2f2d6f3b813909be59dcabc9f93ec40
Vladislav <svialence@yandex.ru>
2022-11-15 17:42:02 +0300

    merge about page in partnership page, improve about section styles

--- client/components/pages/account/partnership/Invitation.tsx
+++ client/components/pages/account/partnership/Invitation.tsx
@@ -1,27 +1,27 @@
 import * as React from "react";
 import Style from "@components/pages/account/partnership/Invitation.module.scss";
-import Link from "next/link";
 import cn from "classnames";
 import FormButton from "@components/common/form/button/Button";
+import About from "@components/pages/account/partnership/about/About";
 
 export const Invitation: React.FC = function () {
   return (
     <div>
+      <h1>Партнёрская программа</h1>
       <p className={Style.info}>
-        Ищите стабильный способ заработка в интернете? Тогда вам повезло. Наш
-        игровой проект предлагает широкий спектр возможностей по заработку. Для
-        этого вам потребуется немного свободного времени, наличие интернета и
-        желание!
-        <div className={"text-end"}>
-          <Link href={"/account/partnership/about"}>
-            <a>Подробнее</a>
-          </Link>
-        </div>
+        Ищите <span className={"fw-semibold"}>стабильный способ заработка</span>{" "}
+        в интернете? Тогда вам повезло. Наш игровой проект предлагает широкий
+        спектр возможностей по заработку. Для этого вам потребуется немного
+        свободного времени, наличие интернета и желание!
       </p>
 
-      <div className={cn("d-flex", "flex-column", "align-items-center")}>
-        <p className={cn(Style.info, "mb-0", "text-center")}>
-          Вы не участвуете в программе партнёрства. <br /> Нажмите «Начать» и
+      <About />
+
+      <div
+        className={cn("d-flex", "flex-column", "align-items-center", "mt-5")}
+      >
+        <p className={cn(Style.info, "mb-0", "text-center", "fw-semibold")}>
+          Вы не участвуете в программе партнёрства. Нажмите «Начать» и
           зарабатывайте вместе с нами.
         </p>
         <FormButton className={cn("mt-2", "w-auto")}>начать</FormButton>
