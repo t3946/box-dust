25c267d66467fdccb4dee8fe63300b0fd6160034
Vladislav <svialence@yandex.ru>
2022-11-24 23:15:17 +0300

    refactor initial data loading

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,6 +6,63 @@ import { SSRProvider } from "react-bootstrap";
 import axios from "axios";
 import NextApp from "next/app";
 
+async function loadCatalogData(baseUrl) {
+  return await axios.get(baseUrl + "/category").then((res) => {
+    return res.data.catalog;
+  });
+}
+
+async function loadStockData(baseUrl, headers) {
+  return await axios
+    .get(baseUrl + "/stock/get", {
+      withCredentials: true,
+      headers,
+    })
+    .then((res) => res.data.stock)
+    .catch(() => {
+      return [];
+    });
+}
+
+async function loadReviewsData(baseUrl) {
+  return await axios.get(baseUrl + "/reviews/1").then((res) => {
+    return res.data.reviews;
+  });
+}
+
+async function loadHistoryPrizesData(baseUrl) {
+  return await axios
+    .get(baseUrl + "/prizes-history/get?take=15")
+    .then((res) => {
+      return res.data.history;
+    });
+}
+
+async function loadUserData(baseUrl, headers) {
+  return await axios
+    .get(baseUrl + "/auth/info", {
+      withCredentials: true,
+      headers,
+    })
+    .then((res) => {
+      return res.data.user;
+    })
+    .catch(() => {
+      return null;
+    });
+}
+
+async function loadPartnershipData(baseUrl, headers) {
+  return await axios
+    .get(baseUrl + "/partnership/get-all", {
+      withCredentials: true,
+      headers,
+    })
+    .then((res) => {
+      return res.data.partnerships;
+    });
+}
+
 function App({ Component, pageProps }) {
   return (
     <Provider store={getStore(pageProps.storeInitialData)}>
@@ -25,63 +82,19 @@ App.getInitialProps = async (appContext) => {
   }
 
   const baseUrl = "http://localhost:3080/api";
-  const catalog = await axios.get(baseUrl + "/category").then((res) => {
-    return res.data.catalog;
-  });
   const headers = {
     cookie: `auth=${appContext.ctx.req?.cookies?.auth}`,
   };
 
-  const historyPrizes = await axios
-    .get(baseUrl + "/prizes-history/get?take=15")
-    .then((res) => {
-      return res.data.history;
-    });
-
-  const reviews = await axios.get(baseUrl + "/reviews/1").then((res) => {
-    return res.data.reviews;
-  });
-
-  const stock = await axios
-    .get(baseUrl + "/stock/get", {
-      withCredentials: true,
-      headers,
-    })
-    .then((res) => res.data.stock)
-    .catch(() => {
-      return [];
-    });
-
-  const user = await axios
-    .get(baseUrl + "/auth/info", {
-      withCredentials: true,
-      headers,
-    })
-    .then((res) => {
-      // log
-      if (typeof window === "undefined") {
-        console.log("SERVER");
-      } else {
-        console.log("CLIENT");
-      }
-      console.log(
-        `Fields: name="${res.data.user.name}" avatar="${res.data.user.avatar}"`
-      );
-
-      return res.data.user;
-    })
-    .catch(() => {
-      return null;
-    });
-
-  const partnerships = await axios
-    .get(baseUrl + "/partnership/get-all", {
-      withCredentials: true,
-      headers,
-    })
-    .then((res) => {
-      return res.data.partnerships;
-    });
+  const [user, stock, reviews, historyPrizes, partnerships, catalog] =
+    await Promise.all([
+      loadUserData(baseUrl, headers),
+      loadStockData(baseUrl),
+      loadReviewsData(baseUrl),
+      loadHistoryPrizesData(baseUrl, headers),
+      loadPartnershipData(baseUrl, headers),
+      loadCatalogData(baseUrl),
+    ]);
 
   appProps.pageProps.storeInitialData = {
     catalog,
