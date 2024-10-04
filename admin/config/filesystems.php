69292f85c7d254d2c93692eef26bff83dca689fa
Vladislav <svialence@yandex.ru>
2024-10-04 03:04:34 +0300

    add s3 storage in project

--- admin/config/filesystems.php
+++ admin/config/filesystems.php
@@ -44,12 +44,11 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'key' => env('YANDEX_ACCESS_KEY_ID'),
+            'secret' => env('YANDEX_SECRET_ACCESS_KEY'),
+            'region' => env('YANDEX_DEFAULT_REGION'),
+            'bucket' => env('YANDEX_BUCKET'),
+            'endpoint' => env('YANDEX_ENDPOINT'),
         ],
 
     ],
